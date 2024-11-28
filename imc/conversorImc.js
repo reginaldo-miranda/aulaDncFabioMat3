21710b70745a55449ade2d9287ea74dae09852a5
reginaldo miranda <reginaldobrain@gmail.com>
2024-11-28 11:44:48 -0300

    a

--- imc/conversorImc.js
+++ imc/conversorImc.js
@@ -1,7 +1,46 @@
 const conversorFator = 0.01;
 
+function classificateImc(imc){
+    if (imc < 18.5){
+        return "Abaixo do peso"
+    } else if (imc >= 18.5 && imc < 25){
+        return "Peso normal"
+    } else if (imc >= 25 && imc > 30){
+        return "sobre peso"
+    } else{
+        return "Obesidade"
+    }
+
+}
+
 
 function calculaImc(person){
 
-    
+    let height = person.height;
+    let weight = person.weight;
+
+    height *= conversorFator;
+
+    let imc = weight / (height * height);
+    const classification  = classificateImc(imc);
+    console.log(classification);
+    const result = "O resultado do IMC é de " + imc;
+    const isHealthy = true;
+
+    return {
+        name: person.name,
+        imc,
+        classification,
+        result,
+        isHealthy,
+    }
+  
 }
+
+const person = {
+    name:'Fabio',
+    weight:80,
+    height: 180,
+    
+} 
+console.log(calculaImc(person));
